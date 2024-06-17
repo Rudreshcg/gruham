d31300d4cd5bc86f9518cfc593a393e39bc285e6
Rudreshcg <rudreshcsbg82@gmail.com>
2024-06-17 22:45:20 +0530

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton, Drawer, List, ListItem, ListItemText } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, Drawer, List, ListItem, ListItemText, Divider } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link as RouterLink } from 'react-router-dom';
+import { styled } from '@mui/system';
+
+// Create a styled component for the desktop menu
+const DesktopMenu = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.up('md')]: {
+    display: 'flex',
+  },
+}));
 
 const Header: React.FC = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -18,26 +27,79 @@ const Header: React.FC = () => {
           backgroundColor: 'white', 
           color: 'black' 
         }} 
-        elevation={1}
+        elevation={0}
       >
-        <Toolbar sx={{ maxWidth: '1200px', margin: '0 auto', width: '100%' }}>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Toolbar sx={{ maxWidth: '1200px', margin: '0 auto', width: '100%', minHeight: '80px', padding: '0 16px' }}>
+          <Typography
+            variant="h6"
+            component={RouterLink}
+            to="/"
+            sx={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}
+          >
             My App
           </Typography>
-          <div className="desktop-menu">
-            <Button color="inherit" component={RouterLink} to="/" sx={{ color: 'black' }}>
+          <DesktopMenu>
+            <Button 
+              color="inherit" 
+              component={RouterLink} 
+              to="/projects" 
+              sx={{ 
+                color: '#4b4f58', 
+                p: "16px 24px",
+                '&:hover': {
+                  color: 'blue', 
+                  backgroundColor: 'inherit'
+                } 
+              }}
+            >
               PROJECTS
             </Button>
-            <Button color="inherit" component={RouterLink} to="/videos" sx={{ color: 'black' }}>
+            <Button 
+              color="inherit" 
+              component={RouterLink} 
+              to="/videos" 
+              sx={{ 
+                color: '#4b4f58', 
+                p: "16px 24px",
+                '&:hover': {
+                  color: 'blue', 
+                  backgroundColor: 'inherit' 
+                } 
+              }}
+            >
               VIDEOS
             </Button>
-            <Button color="inherit" component={RouterLink} to="/about" sx={{ color: 'black' }}>
+            <Button 
+              color="inherit" 
+              component={RouterLink} 
+              to="/about" 
+              sx={{ 
+                color: '#4b4f58', 
+                p: "16px 24px",
+                '&:hover': {
+                  color: 'blue', 
+                  backgroundColor: 'inherit' 
+                } 
+              }}
+            >
               ABOUT
             </Button>
-            <Button color="inherit" component={RouterLink} to="/contact" sx={{ color: 'black' }}>
+            <Button 
+              color="inherit" 
+              component={RouterLink} 
+              to="/contact" 
+              sx={{ 
+                color: '#4b4f58', 
+                p: "16px 24px",
+                '&:hover': {
+                  color: 'blue', 
+                  backgroundColor: 'inherit' 
+                } 
+              }}
+            >
               CONTACT
             </Button>
-          </div>
+          </DesktopMenu>
           <IconButton 
             edge="start" 
             color="inherit" 
@@ -48,6 +110,7 @@ const Header: React.FC = () => {
             <MenuIcon />
           </IconButton>
         </Toolbar>
+        <Divider sx={{ width: '100%' }} />
       </AppBar>
 
       <Drawer 
@@ -57,7 +120,7 @@ const Header: React.FC = () => {
         sx={{ display: { xs: 'block', md: 'none' } }}
       >
         <List sx={{ width: 250 }}>
-          <ListItem button component={RouterLink} to="/" onClick={toggleDrawer(false)}>
+          <ListItem button component={RouterLink} to="/projects" onClick={toggleDrawer(false)}>
             <ListItemText primary="PROJECTS" />
           </ListItem>
           <ListItem button component={RouterLink} to="/videos" onClick={toggleDrawer(false)}>
